f665e79021f6c1db506f89d5e289107764d32a5c
AnitaAO <anitaisbeta@gmail.com>
2023-07-13 13:25:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
-* a comment
-*/
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle);
 return (0);
 }
