caa391b989a688e0e16d6493c9dca9c725e60256
AnitaAO <anitaisbeta@gmail.com>
2023-07-14 16:30:47 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alpahbets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program that prints lower and upper alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+             int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
