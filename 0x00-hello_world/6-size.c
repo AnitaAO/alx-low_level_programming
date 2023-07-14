c30e515bc0f435b4f6747b60979dd510caff9478
AnitaAO <anitaisbeta@gmail.com>
2023-07-14 04:50:41 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,15 @@
  */
 int main(void)
 {
-printf("size of a char: %zu byte(s)\n", sizeof(char));
-printf("size of an int: %zu byte(s)\n", sizeof(int));
-printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("size of a long long int: %zu byte(s)\n", sizeof(int64_t));
-printf("size of a float: %zu byte(s)\n", sizeof(float));
+printf
+	("size of a char: %zu byte(s)\n", sizeof(char));
+printf
+	("size of an int: %zu byte(s)\n", sizeof(int));
+printf
+	("size of a long int: %zu byte(s)\n", sizeof(long int));
+printf
+	("size of a long long int: %zu byte(s)\n", sizeof(int64_t));
+printf
+	("size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
