80383e43b4bb62498027e58f3c60ba90426e9a95
AnitaAO <anitaisbeta@gmail.com>
2023-07-14 18:45:55 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Prints all lowercase of base 16
+ * Return: 0
+ */
+int main(void)
+{
+int n, m, i;
+for (n = 48; n <= 58; n++)
+{
+for (m = 49; m <= 58; m++)
+{
+for (i = 50; i <= 58; i++)
+{
+if (i > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(i);
+if (n != 55 || m != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
