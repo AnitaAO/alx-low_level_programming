402766395d9bbde5ca3cd1f3f7d657a0595c46b1
AnitaAO <anitaisbeta@gmail.com>
2023-07-14 18:52:15 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Prints all lowercase of base 16
+ * main - Prints all different combinations of 3 digits
  * Return: 0
  */
 int main(void)
 {
 int n, m, i;
-for (n = 48; n <= 58; n++)
+for (n = 48; n < 58; n++)
 {
-for (m = 49; m <= 58; m++)
+for (m = 49; m < 58; m++)
 {
-for (i = 50; i <= 58; i++)
+for (i = 50; i < 58; i++)
 {
 if (i > m && m > n)
 {
