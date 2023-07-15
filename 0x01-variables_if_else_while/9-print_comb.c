dbd31809fe7da57bd74f6a54a613b962325171b2
AnitaAO <anitaisbeta@gmail.com>
2023-07-14 17:49:34 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints all lowercase of base 16
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+if (n != 57)
+{
+putchar('.');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
