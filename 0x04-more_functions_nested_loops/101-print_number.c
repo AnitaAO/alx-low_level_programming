47e725ac560a36383de82097bd17ddf871f80747
AnitaAO <anitaisbeta@gmail.com>
2023-07-21 14:21:58 -0700

    101-print_numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n:.input intger parameter
+ * Return: 0 Always
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
