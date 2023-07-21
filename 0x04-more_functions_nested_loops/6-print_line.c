2055125fee68cd40caa19d73f7b98e654628b2bf
AnitaAO <anitaisbeta@gmail.com>
2023-07-21 08:06:33 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,10 +2,17 @@
 
 /**
  * print_line - draws a straight line
- *
+ *@n:No of times
  * Return: 0
  */
 
 void print_line(int n)
 {
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
