197805d1fa7063cab68ff4e5a57faed625bb2e2b
AnitaAO <anitaisbeta@gmail.com>
2023-07-21 07:56:51 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,7 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			_putchar('1');
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+
