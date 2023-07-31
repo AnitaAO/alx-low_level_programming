5bfe7c2a8b3b8fa3f15993068e0c9dd0d37287c4
AnitaAO <anitaisbeta@gmail.com>
2023-07-31 10:41:04 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset (char *s, char b, unsigned int n)
 {
 	int i = 0;
 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
