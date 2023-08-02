6a9724127adc3a7800bcc6c4a5becc22a8f8713b
AnitaAO <anitaisbeta@gmail.com>
2023-08-02 07:01:23 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: 0 Always
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,11 +3,11 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: character to be printed
- * Return: 1 on success
- * On error -1 is returned, and errno is set appropriately 
+ * Return: on success 1
+ * On error -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
 {
-	return(write((1, &c, 1)));
+        return((write(1, &c, 1)));
 }
