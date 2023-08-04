552ddfbdd1a645160eb0a046ded719c8c4acbca1
AnitaAO <anitaisbeta@gmail.com>
2023-08-04 06:32:25 -0700

    added all c files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if upper case letters
+ * @c: char to check
+ *
+ * Return: if uppercase return 1, else return o
+ */
+
+int _isupper(int c)
+{
+	if (b >= 'A' && b <= 'Z')
+		return (1);
+	return (0);
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Program fills a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * * Return: change array with new value for n bytes
+ */
+
+char *_memset(char *s, char c, unsigned int n)
+{
+	int k = 0;
+	
+	for (k = 0; n > 0; k++)
+	{
+		s[k] = b;
+		n--;
+	}
+	return (s);
+}
